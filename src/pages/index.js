57987c98eb2a15584b8341669e9ae688cbdcdc64
JulianMayorga <mayorga.julian@gmail.com>
2018-02-13 16:56:42 -0300

    Make post links blue in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,10 +55,7 @@ class BlogIndex extends React.Component {
                       marginBottom: rhythm(1 / 4),
                     }}
                   >
-                    <Link
-                      style={{ boxShadow: 'none', color: '#333' }}
-                      to={node.fields.slug}
-                    >
+                    <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
                       {title}
                     </Link>
                   </h3>
