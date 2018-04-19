a7c2cc11988e195affa01c9a1c72509618c5eaf7
JulianMayorga <mayorga.julian@gmail.com>
2018-04-19 10:55:29 -0300

    Allow async await in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   "parserOptions": {
     "sourceType": "module",
+    "ecmaVersion": 2017,
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
       "jsx": true,
