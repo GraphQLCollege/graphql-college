7480dc5d60627a22b94106011264be0d8bb65772
JulianMayorga <mayorga.julian@gmail.com>
2018-06-05 13:30:45 -0300

    Add more info to book landing page

--- src/pages/fullstack-graphql.js
+++ src/pages/fullstack-graphql.js
@@ -7,6 +7,7 @@ import { rhythm } from '../utils/typography'
 import Book from '../components/book-perspective.svg'
 import Button from '../components/Button'
 import SubscribeForm from '../components/SubscribeForm'
+import profilePic from '../components/profile-pic.png'
 
 import './fullstack-graphql.css'
 
@@ -60,6 +61,58 @@ class FullstackGraphQL extends React.Component {
           </div>
         </div>
         <SubscribeForm text="Subscribe to receive book updates" />
+        <div className="what-you-will-build">
+          <h2>What you'll build</h2>
+          <img src="/pinapp.gif" />
+          <p>You will build from scratch a Pinterest clone called PinApp</p>
+          <p>
+            Every chapter will slowly teach you the different moving parts of a
+            GraphQL stack.
+          </p>
+          <p>
+            The <b>first chapter</b> will teach you how to read and write data
+            from PinApp, like fetching users or pins, using the GraphQL query
+            language.
+          </p>
+          <p>
+            The <b>second chapter</b> will teach you how to design PinApp's data
+            model using the GraphQL schema design language.
+          </p>
+          <p>
+            In the <b>third chapter</b> you will learn how to create PinApp's
+            GraphQL APIs using{' '}
+            <a
+              href="https://www.apollographql.com/docs/apollo-server/"
+              target="_blank"
+            >
+              Apollo Server
+            </a>. You will expose to HTTP the schema you designed in the
+            previous chapter. You will also learn how to connect an API with a
+            Postgres database, and how to organize the API source code.
+          </p>
+          <p>
+            The <b>fourth chapter</b> teaches you how to build PinApp's frontend
+            using{' '}
+            <a target="_blank" href="https://www.apollographql.com/client">
+              Apollo Client 2.0
+            </a>{' '}
+            and{' '}
+            <a href="https://reactjs.org/" target="_blank">
+              React 16
+            </a>.
+          </p>
+          <p>
+            In the <b>fifth chapter</b> you will learn how to add real time
+            features to PinApp using GraphQL Subscriptions.
+          </p>
+          <p>
+            The <b>sixth chapter</b> will teach you how to test PinApp's API and
+            frontend using{' '}
+            <a href="http://facebook.github.io/jest/" target="_blank">
+              Jest
+            </a>.
+          </p>
+        </div>
         <div className="chapters">
           <h2>Chapters</h2>
           <ul>
@@ -210,6 +263,52 @@ class FullstackGraphQL extends React.Component {
               </div>
             </li>
           </ul>
+        </div>
+        <div className="about-author">
+          <h2>Meet the author</h2>
+          <p>
+            Hi there! I am <b>Julian Mayorga</b>. I have been a full stack
+            Javascript developer since 2013, working in a wide range of tech
+            companies. From a payments startup to a digital car inspections
+            company.
+          </p>
+          <p>
+            Building beautiful frontends and APIs is my passion. And when I say
+            beautiful, I mean both external as internal. Nowadays I am totally
+            obsessed over GraphQL and React. Using them I feel like I can create
+            lovely, maintainable products. I truly enjoy sharing this passion
+            about building apps by writing at GraphQL College.
+          </p>
+          <img src={profilePic} alt={`Julian Mayorga - Author`} />
+        </div>
+        <div className="final-words">
+          <p>
+            Interested?{' '}
+            <a
+              style={{
+                boxShadow: 'none',
+                textDecoration: 'underline',
+                color: '#e535ab',
+              }}
+              href=""
+              target="_blank"
+              href="/fullstack-graphql-sample.zip"
+            >
+              Download the sample chapter
+            </a>{' '}
+            to get a taste of the book. If you have any questions, I would be
+            very happy to answer them, so please send me an email to{' '}
+            <a
+              style={{
+                boxShadow: 'none',
+                textDecoration: 'underline',
+                color: '#e535ab',
+              }}
+              href="mailto:julian@graphql.college"
+            >
+              julian@graphql.college
+            </a>
+          </p>
         </div>
       </div>
     )
