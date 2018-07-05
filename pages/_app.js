0fe6aa0508e4d07170d9e961d4031d294d2d6684
JulianMayorga <mayorga.julian@gmail.com>
2018-07-05 19:24:47 -0300

    Delete service workers on every page
    Previous version of GraphQL College registered a service worker.
    
    This service worker is breaking the new version, so we uninstall it.

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import React from "react";
 import App, { Container } from "next/app";
 
 import Layout from "../components/Layout";
+import { deleteServiceWorkers } from "../utils/offline";
 
-import "./index.css"
+import "./index.css";
 
 export default class MyApp extends App {
+  componentDidMount() {
+    deleteServiceWorkers();
+  }
   render() {
     const { Component, pageProps } = this.props;
     return (
