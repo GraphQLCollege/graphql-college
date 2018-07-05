95538d7ae53a8dcc0ee080e1658442c190f5e7cc
JulianMayorga <mayorga.julian@gmail.com>
2018-07-05 19:35:47 -0300

    TEMP: Burger

---
+++ components/Burger.js
@@ -0,0 +1,272 @@
+import React from "react";
+import { Engine, Render, World, Bodies } from "matter-js";
+
+const selectPatty = burger => {
+  const beef = () =>
+    Bodies.rectangle(150, 40, 220, 30, {
+      render: {
+        sprite: {
+          texture: "/static/images/beef.png"
+        }
+      }
+    });
+  const chicken = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/chicken.png"
+      }
+    }
+  });
+  const mushroom = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/mushrooms.png"
+      }
+    }
+  });
+  const sauteedVegetables = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/sauteed-vegetables.png"
+      }
+    }
+  });
+  const beefCheese = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/beef-cheese.png"
+      }
+    }
+  });
+  const chickenCheese = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/chicken-cheese.png"
+      }
+    }
+  });
+  const mushroomCheese = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/mushrooms-cheese.png"
+      }
+    }
+  });
+  const sauteedVegetablesCheese = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/sauteed-vegetables-cheese.png"
+      }
+    }
+  });
+  const beefCheddar = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/beef-cheddar.png"
+      }
+    }
+  });
+  const chickenCheddar = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/chicken-cheddar.png"
+      }
+    }
+  });
+  const mushroomCheddar = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/mushrooms-cheddar.png"
+      }
+    }
+  });
+  const sauteedVegetablesCheddar = Bodies.rectangle(150, 40, 220, 30, {
+    render: {
+      sprite: {
+        texture: "/static/images/sauteed-vegetables-cheddar.png"
+      }
+    }
+  });
+  const patties = {
+    meat: beef,
+    chicken,
+    mushroom,
+    sauteedVegetables
+  };
+  const cheesePatties = {
+    meat: beefCheese,
+    chicken: chickenCheese,
+    mushroom: mushroomCheese,
+    sauteedVegetables: sauteedVegetablesCheese
+  };
+  const cheddarPatties = {
+    meat: beefCheddar,
+    chicken: chickenCheddar,
+    mushroom: mushroomCheddar,
+    sauteedVegetables: sauteedVegetablesCheddar
+  };
+  if (burger.topping) {
+    if (burger.topping.cheese === "simple") {
+      if (burger.patty === "doubleMeat") {
+        return [beef(), cheesePatties["meat"]];
+      }
+      if (burger.patty === "tripleMeat") {
+        return [beef(), beef(), cheesePatties["meat"]];
+      }
+      return [cheesePatties[burger.patty]];
+    }
+    if (burger.topping.cheese === "cheddar") {
+      if (burger.patty === "doubleMeat") {
+        return [beef(), cheddarPatties["meat"]];
+      }
+      if (burger.patty === "tripleMeat") {
+        return [beef(), beef(), cheddarPatties["meat"]];
+      }
+      return [cheddarPatties[burger.patty]];
+    }
+  }
+  if (burger.patty === "doubleMeat") {
+    return [beef(), beef()];
+  }
+  if (burger.patty === "tripleMeat") {
+    return [beef(), beef(), beef()];
+  }
+  return [patties[burger.patty]];
+};
+
+const selectTopBun = burger => {
+  const topBun = Bodies.rectangle(150, -100, 200, 60, {
+    render: {
+      sprite: {
+        texture: "/static/images/top-bun.png"
+      }
+    }
+  });
+  const topBunSesame = Bodies.rectangle(150, -100, 200, 60, {
+    render: {
+      sprite: {
+        texture: "/static/images/top-bun-sesame.png"
+      }
+    }
+  });
+  const buns = {
+    simple: topBun,
+    sesame: topBunSesame
+  };
+  return buns[burger.bun];
+};
+
+const selectToppings = burger => {
+  const tomato = Bodies.rectangle(150, -90, 180, 15, {
+    render: {
+      sprite: {
+        texture: "/static/images/tomato.png"
+      }
+    }
+  });
+  const lettuce = Bodies.rectangle(150, -90, 180, 15, {
+    render: {
+      sprite: {
+        texture: "/static/images/lettuce.png"
+      }
+    }
+  });
+  const bacon = Bodies.rectangle(150, -90, 180, 15, {
+    render: {
+      sprite: {
+        texture: "/static/images/bacon.png"
+      }
+    }
+  });
+  const egg = Bodies.rectangle(150, -90, 180, 15, {
+    render: {
+      sprite: {
+        texture: "/static/images/egg.png"
+      }
+    }
+  });
+
+  const toppings = {
+    tomato,
+    lettuce,
+    bacon,
+    egg
+  };
+  return Object.keys(burger.topping)
+    .filter(topping => topping !== "cheese")
+    .map(topping => {
+      return toppings[topping];
+    });
+};
+
+class Burger extends React.Component {
+  componentDidMount() {
+    this.matterEngine = Engine.create();
+
+    this.matterRender = Render.create({
+      canvas: this.element,
+      engine: this.matterEngine,
+      options: {
+        wireframes: false,
+        width: 300,
+        height: 300,
+        background: "transparent"
+      }
+    });
+
+    this.showBurger();
+  }
+  componentDidUpdate() {
+    this.showBurger();
+  }
+  showBurger = () => {
+    const bottomBun = Bodies.rectangle(150, 50, 200, 25, {
+      render: {
+        sprite: {
+          texture: "/static/images/bottom-bun.png"
+        }
+      }
+    });
+    const ground = Bodies.rectangle(150, 300, 810, 60, {
+      isStatic: true,
+      render: {
+        fillStyle: "#f6f7f8"
+      }
+    });
+    let bodies = [ground];
+
+    if (this.props.burger) {
+      if (this.props.burger.topping) {
+        bodies = bodies.concat(selectToppings(this.props.burger));
+      }
+      bodies = bodies.concat([
+        selectTopBun(this.props.burger),
+        ...selectPatty(this.props.burger),
+        bottomBun
+      ]);
+    }
+
+    Render.stop(this.matterRender);
+    World.clear(this.matterEngine.world);
+    Engine.clear(this.matterEngine);
+
+    World.add(this.matterEngine.world, bodies);
+
+    Engine.run(this.matterEngine);
+
+    Render.run(this.matterRender);
+  };
+  render() {
+    return (
+      <div className="burger">
+        <canvas
+          ref={element => {
+            this.element = element;
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Burger;
---
+++ components/GraphQLBurger.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import dynamic from "next/dynamic";
+import { graphql } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import NoSSR from "react-no-ssr";
+import styled from "styled-components";
+
+import "graphiql/graphiql.css";
+import Burger from "./Burger";
+
+const GraphiQL = dynamic(import("graphiql"), {
+  ssr: false
+});
+
+const typeDefs = `
+type Query {
+  burger: Burger!
+}
+
+type Burger {
+  bun(type: Bun!): Bun
+  patty(type: Patty!): Patty
+  topping: Topping
+}
+
+enum Bun {
+  simple
+  sesame
+}
+
+enum Patty {
+  meat
+  doubleMeat
+  tripleMeat
+  chicken
+  mushroom
+  sauteedVegetables
+}
+
+enum Cheese {
+  simple
+  cheddar
+}
+
+type Topping {
+  cheese(type: Cheese!): Cheese
+  egg: Boolean
+  lettuce: Boolean
+  tomato: Boolean
+  bacon: Boolean
+}
+`;
+
+const resolvers = {
+  Query: {
+    burger: () => ({})
+  },
+  Burger: {
+    bun: (_, { type }) => type,
+    patty: (_, { type }) => type,
+    topping: () => ({})
+  },
+  Topping: {
+    cheese: (a, { type }) => type
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+
+  .graphiql-container {
+    height: 300px;
+  }
+
+  @media(min-width: 600px) {
+    flex-direction: row;
+  }
+`;
+
+class App extends Component {
+  state = { burger: null };
+  render() {
+    return (
+      <Wrapper>
+        <NoSSR>
+          <GraphiQL
+            fetcher={graphQLParams => {
+              return graphql(schema, graphQLParams.query).then(res => {
+                this.setState({ burger: res.data.burger });
+                return res;
+              });
+            }}
+            schema={schema}
+          />
+        </NoSSR>
+        <Burger burger={this.state.burger} />
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
