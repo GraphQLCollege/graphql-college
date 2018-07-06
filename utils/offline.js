32a2f3d9be87f6dd9304df047768e079e68b640d
JulianMayorga <mayorga.julian@gmail.com>
2018-07-06 09:45:56 -0300

    Remove delete service workers hack. Keep webpack plugin

--- utils/offline.js
+++
@@ -1,7 +0,0 @@
-export const deleteServiceWorkers = () => {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
-};
