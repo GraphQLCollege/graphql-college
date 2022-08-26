55650eaee1cca4ede39118546e5b9b583521ab4c
Julian <mayorga.julian@gmail.com>
2022-08-26 17:03:42 -0300

    Redirect /fullstack-graphql to graphqladmin.com/books/fullstack-graphql

--- next.config.js
+++ next.config.js
@@ -10,15 +10,15 @@ const withMDX = require("@zeit/next-mdx")({
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
   // https://nextjs.org/docs/api-reference/next.config.js/redirects
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/fullstack-graphql",
-  //       destination: "https://gql.app/books/fullstack-graphql",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/fullstack-graphql",
+        destination: "https://graphqladmin.com/books/fullstack-graphql",
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
